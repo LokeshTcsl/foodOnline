6f50da3579db7c7a9dd9055185360f9a7595966a
LokeshTcsl <lokeshiiitn2020@gmail.com>
2023-06-21 20:13:06 +0530

    Forgot Password feature

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -140,3 +140,13 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     
 }
+
+
+# Email configuration
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+DEFAULT_FROM_EMAIL = 'foodOnline MarketPlace <lokeshiiitn1999@gmail.com>'
